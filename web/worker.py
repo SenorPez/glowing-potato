ec16c12cffbe6cf48fe3e247624ee5d0cd51aa03
SenorPez <senorpez@senorpez.com>
2019-02-15 20:39:36 +0000

    Linted worker.py

--- web/worker.py
+++ web/worker.py
@@ -1,40 +1,90 @@
 from datetime import datetime
-from math import sqrt, pi
+from math import sqrt
 import numpy as np
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
 import requests
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+from pykep import AU, DAY2SEC, SEC2DAY, epoch, lambert_problem, epoch_from_string
+from pykep.planet import keplerian as planet
 
-from pykep import AU, DAY2SEC, SEC2DAY, epoch, lambert_problem, propagate_taylor, sims_flanagan, MU_SUN, epoch_from_string
-from pykep.planet import keplerian as planet, jpl_lp
-from pykep.orbit_plots import plot_planet, plot_lambert, plot_kepler, plot_taylor, plot_sf_leg
+def create_planet(planet_name, planet_color, star_link, gm_star):
+    # Get planetary mass, radius, GM, and planet object of 1 Eta Veneris 3
+    planet_link = find_link_by_name(
+        star_link + "/planets",
+        "trident-api:planet",
+        planet_name)
+    req = requests.get(planet_link)
+    req.raise_for_status()
 
-# Get standard solar mass.
-req = requests.get("http://trident.senorpez.com/constants/Msol")
-req.raise_for_status()
-mass_solar = req.json()['value']
+    mass = req.json()['mass'] * MASS_PLANET
+    radius = req.json()['radius'] * RADIUS_PLANET
+    gm = mass * GRAV
 
-# Get gravitational constant.
-req = requests.get("http://trident.senorpez.com/constants/G")
-req.raise_for_status()
-grav = req.json()['value']
+    return_planet = planet(
+        epoch(0),
+        (
+            req.json()['semimajorAxis'] * AU,
+            req.json()['eccentricity'],
+            req.json()['inclination'],
+            req.json()['longitudeOfAscendingNode'],
+            req.json()['argumentOfPeriapsis'],
+            req.json()['trueAnomalyAtEpoch']),
+        gm_star,
+        gm,
+        radius,
+        radius,
+        planet_name)
+    return_planet.color = planet_color
 
-# Get standard planetary mass.
-req = requests.get("http://trident.senorpez.com/constants/Mpln")
-req.raise_for_status()
-mass_planet = req.json()['value']
+    return return_planet
 
-# Get standard planetary equatorial radius.
-req = requests.get("http://trident.senorpez.com/constants/Rpln")
-req.raise_for_status()
-radius_planet = req.json()['value']
+def get_constant(constant_name):
+    req = requests.get("http://trident.senorpez.com/constants/" + str(constant_name))
+    req.raise_for_status()
+    return req.json()['value']
 
-APP = Flask(__name__)
-CORS(APP, resources={r"/*": {"origins": "http://senorpez.com"}})
+def find_link_by_name(api_link, collection_name, target_name):
+    req = requests.get(api_link)
+    req.raise_for_status()
+    link = None
+    for entry in req.json()['_embedded'][collection_name]:
+        if entry['name'] == target_name:
+            link = entry['_links']['self']['href']
+    return link
+
+def find_planet_by_id(planet_id):
+    planet_data = requests.get(
+        "http://trident.senorpez.com/systems/1817514095/stars/1905216634/planets/"
+        + str(planet_id))
+    planet_data.raise_for_status()
+    return planet_data
+
+def get_planet(planet_id, planet_color, gm_star):
+    planet_data = find_planet_by_id(planet_id)
+
+    mass = planet_data.json()['mass'] * MASS_PLANET
+    radius = planet_data.json()['radius'] * RADIUS_PLANET
+    gm = mass * GRAV
+
+    return_planet = planet(
+        epoch(0),
+        (
+            planet_data.json()['semimajorAxis'] * AU,
+            planet_data.json()['eccentricity'],
+            planet_data.json()['inclination'],
+            planet_data.json()['longitudeOfAscendingNode'],
+            planet_data.json()['argumentOfPeriapsis'],
+            planet_data.json()['trueAnomalyAtEpoch']),
+        gm_star,
+        gm,
+        radius,
+        radius,
+        planet_data.json()['name'])
+
+    return_planet.color = planet_color
+
+    return return_planet
 
 def get_star(system_name, star_name):
     system_link = find_link_by_name(
@@ -49,14 +99,29 @@ def get_star(system_name, star_name):
     req = requests.get(star_link)
     req.raise_for_status()
 
-    mass = req.json()['solarMass'] * mass_solar
-    gm = mass * grav
+    mass = req.json()['solarMass'] * MASS_SOLAR
+    gm = mass * GRAV
 
-    return (star_link, mass, gm)
+    return (star_link, gm)
+
+# Get standard solar mass.
+MASS_SOLAR = get_constant("Msol")
+
+# Get GRAVitational constant.
+GRAV = get_constant("G")
+
+# Get standard planetary mass.
+MASS_PLANET = get_constant("Mpln")
+
+# Get standard planetary equatorial radius.
+RADIUS_PLANET = get_constant("Rpln")
+
+APP = Flask(__name__)
+CORS(APP, resources={r"/*": {"origins": "http://senorpez.com"}})
 
 @APP.route("/orbit", methods=['GET'])
 def orbit():
-    star_link, mass_s1, gm_s1 = get_star("Eta Veneris", "1 Eta Veneris")
+    star_link, gm_s1 = get_star("Eta Veneris", "1 Eta Veneris")
 
     planets = [
         create_planet(
@@ -69,7 +134,7 @@ def orbit():
             "grey",
             star_link,
             gm_s1),
-	 create_planet(
+        create_planet(
             "1 Eta Veneris 3",
             "green",
             star_link,
@@ -121,17 +186,17 @@ def orbit():
 
 @APP.route("/transfer", methods=['GET'])
 def transfer():
-    star_link, mass_s1, gm_s1 = get_star("Eta Veneris", "1 Eta Veneris")
+    star_link, gm_s1 = get_star("Eta Veneris", "1 Eta Veneris")
     origin = create_planet(
-            "1 Eta Veneris 3",
-            "green",
-            star_link,
-            gm_s1)
+        "1 Eta Veneris 3",
+        "green",
+        star_link,
+        gm_s1)
     target = create_planet(
-            "1 Eta Veneris 4",
-            "grey",
-            star_link,
-            gm_s1)
+        "1 Eta Veneris 4",
+        "grey",
+        star_link,
+        gm_s1)
 
     origin_orbit_radius = origin.radius + 200000
     target_orbit_radius = target.radius + 200000
@@ -139,22 +204,22 @@ def transfer():
     t0 = epoch_from_string(str(datetime.now()))
     t0_number = int(t0.mjd2000)
 
-    FLIGHT_TIMES = np.array(range(50, 251))
-    FLIGHT_TIME_OFFSET = 50
-    LAUNCH_TIMES = np.array(range(t0_number, t0_number + 501))
-    LAUNCH_TIME_OFFSET = t0_number
-    DELTA_V = np.empty((len(FLIGHT_TIMES), len(LAUNCH_TIMES)))
+    flight_times = np.array(range(50, 251))
+    flight_time_offset = 50
+    launch_times = np.array(range(t0_number, t0_number + 501))
+    launch_time_offset = t0_number
+    delta_v = np.empty((len(flight_times), len(launch_times)))
 
-    for launch_time in LAUNCH_TIMES:
-        for flight_time in FLIGHT_TIMES:
+    for launch_time in launch_times:
+        for flight_time in flight_times:
             launch_time = int(launch_time)
             flight_time = int(flight_time)
 
-            T1 = epoch(launch_time)
-            T2 = epoch(launch_time + flight_time)
-            dt = (T2.mjd - T1.mjd)*DAY2SEC
-            r1, v1 = origin.eph(T1)
-            r2, v2 = target.eph(T2)
+            t1 = epoch(launch_time)
+            t2 = epoch(launch_time + flight_time)
+            dt = (t2.mjd - t1.mjd)*DAY2SEC
+            r1, v1 = origin.eph(t1)
+            r2, v2 = target.eph(t2)
             l = lambert_problem(list(r1), list(r2), dt, gm_s1)
 
             delta_vs = list()
@@ -176,83 +241,9 @@ def transfer():
 
                 delta_vs.append(inj_delta_v + ins_delta_v)
 
-            DELTA_V[flight_time-FLIGHT_TIME_OFFSET, launch_time-LAUNCH_TIME_OFFSET] = min(delta_vs)
+            delta_v[flight_time-flight_time_offset, launch_time-launch_time_offset] = min(delta_vs)
 
-    return jsonify(success=True, delta_v=DELTA_V.tolist())
-
-
-def find_link_by_name(api_link, collection_name, target_name):
-    req = requests.get(api_link)
-    req.raise_for_status()
-    link = None
-    for entry in req.json()['_embedded'][collection_name]:
-        if entry['name'] == target_name:
-            link = entry['_links']['self']['href']
-    return link
-
-def find_planet_by_id(planet_id):
-    planet_data = requests.get(
-        "http://trident.senorpez.com/systems/1817514095/stars/1905216634/planets/"
-        + str(planet_id))
-    planet_data.raise_for_status()
-    return planet_data
-
-def get_planet(planet_id, planet_color, gm_star):
-    planet_data = find_planet_by_id(planet_id)
-
-    mass = planet_data.json()['mass'] * mass_planet
-    radius = planet_data.json()['radius'] * radius_planet
-    gm = mass * grav
-
-    return_planet = planet(
-        epoch(0),
-        (
-            planet_data.json()['semimajorAxis'] * AU,
-            planet_data.json()['eccentricity'],
-            planet_data.json()['inclination'],
-            planet_data.json()['longitudeOfAscendingNode'],
-            planet_data.json()['argumentOfPeriapsis'],
-            planet_data.json()['trueAnomalyAtEpoch']),
-        gm_star,
-        gm,
-        radius,
-        radius,
-        planet_data.json()['name'])
-
-    return_planet.color = planet_color
-
-    return return_planet
-
-def create_planet(planet_name, planet_color, star_link, gm_star):
-    # Get planetary mass, radius, GM, and planet object of 1 Eta Veneris 3
-    planet_link = find_link_by_name(
-        star_link + "/planets",
-        "trident-api:planet",
-        planet_name)
-    req = requests.get(planet_link)
-    req.raise_for_status()
-
-    mass = req.json()['mass'] * mass_planet
-    radius = req.json()['radius'] * radius_planet
-    gm = mass * grav
-
-    return_planet = planet(
-        epoch(0),
-        (
-            req.json()['semimajorAxis'] * AU,
-            req.json()['eccentricity'],
-            req.json()['inclination'],
-            req.json()['longitudeOfAscendingNode'],
-            req.json()['argumentOfPeriapsis'],
-            req.json()['trueAnomalyAtEpoch']),
-        gm_star,
-        gm,
-        radius,
-        radius,
-        planet_name)
-    return_planet.color = planet_color
-
-    return return_planet
+    return jsonify(success=True, delta_v=delta_v.tolist())
 
 if __name__ == "__main__":
     APP.run(host="0.0.0.0", port=5001)
