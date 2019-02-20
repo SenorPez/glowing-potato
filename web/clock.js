facb4b0e9f6accb83817dbfc837267358df9040f
SenorPez <senorpez@senorpez.com>
2019-02-20 19:28:37 +0000

    Refactored file. Still horrific.

--- web/clock.js
+++ web/clock.js
@@ -42,180 +42,112 @@ function getTime() {
   var caste = 0
   var festival_day = false;
 
-  while (true) {
-    if (local_days_countdown < 1) {
-      festival_day = true;
-      break;
-    }
-    if (local_days_countdown < 20) {
-      caste = 1;
-      local_days_countdown -= 1;
-      break;
-    }
-    if (local_days_countdown < 41) {
-      caste = 2;
-      local_days_countdown -= 20;
-      break;
-    }
-    if (year % 3) {
-      if (local_days_countdown < 61) {
-        caste = 3;
-        local_days_countdown -= 41;
-        break;
-      }
-    } else {
-      if (local_days_countdown < 51) {
-        caste = 3;
-        local_days_countdown -= 41;
-        break;
-      }
-      if (local_days_countdown < 52) {
-        caste = 3;
-        festival_day = true;
-        local_days_countdown -= 51;
-        break;
-      }
-      if (local_days_countdown < 62) {
-        caste = 3;
-        local_days_countdown -= 52;
-        break;
-      }
-      local_days_countdown -= 1;
-    }
-    if (local_days_countdown < 80) {
-      caste = 4
-      local_days_countdown -= 61;
-      break;
-    }
-    if (local_days_countdown < 99) {
-      caste = 5
-      local_days_countdown -= 80;
-      break;
-    }
-    break;
+  if (local_days_countdown < 1) {
+    festival_day = true;
+  } else if (local_days_countdown < 20) {
+    caste = 1;
+    local_days_countdown -= 1;
+  } else if (local_days_countdown < 41) {
+    caste = 2;
+    local_days_countdown -= 20;
+  } else if (year % 3 && local_days_countdown < 61) {
+    caste = 3;
+    local_days_countdown -= 41;
+  } else if (local_days_countdown < 51) {
+    caste = 3;
+    local_days_countdown -= 41;
+  } else if (local_days_countdown < 52) {
+    caste = 3;
+    festival_day = true;
+    local_days_countdown -= 51;
+  } else if (local_days_countdown < 62) {
+    caste = 3;
+    local_days_countdown -= 52;
+  } else if (year % 3 === 0) {
+    local_days_countdown -= 1;
+  } else if (local_days_countdown < 80) {
+    caste = 4
+    local_days_countdown -= 61;
+  } else if (local_days_countdown < 99) {
+    caste = 5
+    local_days_countdown -= 80;
+  } else {
+    caste = null;
+    local_days_countdown = null;
   }
 
   var day = Math.ceil(local_days_countdown);
   local_days_countdown %= 1;
-
-  var floored = Math.floor((1 + local_days_countdown / 0.25) * 100) / 100;
-  var shift = floored;
   var rawshift = (1 + local_days_countdown / 0.25);
+  var shift = Math.floor(rawshift * 100) / 100;
 
-  var year = year + " FY";
+  var dispYear = year + " FY";
+  var dispCaste = "";
 
   if (festival_day && caste == 0) {
-    caste = "Festival";
+    dispCaste = "Festival";
   } else if (festival_day) {
-    caste = "Midfestival";
+    dispCaste = "Midfestival";
   } else {
-    caste = caste + " Caste"
+    dispCaste = caste + " Caste"
   }
 
-  var day = day + " Day";
-  var shiftNum = Math.floor(shift) - 1;
-  var shift = shift + " Shift";
+  var dispDay = day + " Day";
+  var dispShift = shift + " Shift";
 
-  document.getElementById("year").innerHTML = year + " " + caste + " " + day + " " + shift;
+  document.getElementById("year").innerHTML = dispYear + " " + dispCaste + " " + dispDay + " " + dispShift;
   document.getElementById("t0").innerHTML = new Date();
   document.getElementById("remainder").innerHTML = local_days;
-
-  var startAngle = 0.25 * Math.PI;
-
-  var decimals = (rawshift+"").split(".")[1].split("");
-
-  var rawShiftAngle = (0.25 + 0.5 * (rawshift - 1)) * Math.PI;
-  var shiftAngle = (0.25 + 0.5 * shiftNum) * Math.PI;
-  var nextShiftAngle = shiftAngle + 0.5 * Math.PI;
-
-  var rawTitheAngle = (0.25 + 2 * (rawshift - Math.floor(rawshift))) * Math.PI;
-  var titheAngle = (0.25 + 2 * decimals[0] / 10) * Math.PI;
-  var nextTitheAngle = titheAngle + 0.2 * Math.PI;
-
-  var rawSubtitheAngle = (0.25 + 2 * (rawshift * 10 - Math.floor(rawshift * 10))) * Math.PI;
-  var subtitheAngle = (0.25 + 2 * decimals[1] / 10) * Math.PI;
-  var nextSubtitheAngle = subtitheAngle + 0.2 * Math.PI;
-
-  var rawSpinnerAngle = (0.25 + 2 * (rawshift * 100 - Math.floor(rawshift * 100))) * Math.PI;
-  var spinnerAngle = (0.25 + 2 * decimals[2] / 10) * Math.PI;
-  var nextSpinnerAngle = spinnerAngle + 0.2 * Math.PI;
 
   var c = document.getElementById("clockface");
   var ctx = c.getContext("2d");
   ctx.clearRect(0, 0, 300, 300);
 
-  ctx.beginPath();
-  ctx.arc(150, 150, 100, startAngle, nextShiftAngle);
-  ctx.lineWidth = 8;
-  ctx.strokeStyle = "#d3d3d3";
-  ctx.stroke();
+  var decimals = (rawshift+"").split(".")[1].split("");
 
-  ctx.beginPath();
-  ctx.arc(150, 150, 100, startAngle, rawShiftAngle);
-  ctx.lineWidth = 10;
-  ctx.strokeStyle = "#aa0000";
-  ctx.stroke();
+  var rawShiftAngle = (0.25 + 0.5 * (rawshift - 1)) * Math.PI;
+  var shiftAngle = (0.25 + 0.5 * (Math.floor(shift) - 1)) * Math.PI;
+  var nextShiftAngle = shiftAngle + 0.5 * Math.PI;
 
-  ctx.beginPath();
-  ctx.arc(150, 150, 100, startAngle, shiftAngle);
-  ctx.lineWidth = 12;
-  ctx.strokeStyle = "#ff0000";
-  ctx.stroke();
+  drawClockArc(ctx, 100, 8, "#d3d3d3", nextShiftAngle);
+  drawClockArc(ctx, 100, 10, "#aa0000", rawShiftAngle);
+  drawClockArc(ctx, 100, 12, "#ff0000", shiftAngle);
 
-  ctx.beginPath();
-  ctx.arc(150, 150, 75, startAngle, nextTitheAngle);
-  ctx.lineWidth = 8;
-  ctx.strokeStyle = "#d3d3d3";
-  ctx.stroke();
+  var rawTitheAngle = (0.25 + 2 * (rawshift - Math.floor(rawshift))) * Math.PI;
+  var titheAngle = (0.25 + 2 * decimals[0] / 10) * Math.PI;
+  var nextTitheAngle = titheAngle + 0.2 * Math.PI;
 
-  ctx.beginPath();
-  ctx.arc(150, 150, 75, startAngle, rawTitheAngle);
-  ctx.lineWidth = 10;
-  ctx.strokeStyle = "#6a9f00";
-  ctx.stroke();
+  drawClockArc(ctx, 75, 8, "#d3d3d3", nextTitheAngle);
+  drawClockArc(ctx, 75, 10, "#6a9f00", rawTitheAngle);
+  drawClockArc(ctx, 75, 12, "#00ff00", titheAngle);
 
-  ctx.beginPath();
-  ctx.arc(150, 150, 75, startAngle, titheAngle);
-  ctx.lineWidth = 12;
-  ctx.strokeStyle = "#00ff00";
-  ctx.stroke();
+  var rawSubtitheAngle = (0.25 + 2 * (rawshift * 10 - Math.floor(rawshift * 10))) * Math.PI;
+  var subtitheAngle = (0.25 + 2 * decimals[1] / 10) * Math.PI;
+  var nextSubtitheAngle = subtitheAngle + 0.2 * Math.PI;
 
-  ctx.beginPath();
-  ctx.arc(150, 150, 50, startAngle, nextSubtitheAngle);
-  ctx.lineWidth = 8;
-  ctx.strokeStyle = "#d3d3d3";
-  ctx.stroke();
+  drawClockArc(ctx, 50, 8, "#d3d3d3", nextSubtitheAngle);
+  drawClockArc(ctx, 50, 10, "#006666", rawSubtitheAngle);
+  drawClockArc(ctx, 50, 12, "#0000ff", subtitheAngle);
 
-  ctx.beginPath();
-  ctx.arc(150, 150, 50, startAngle, rawSubtitheAngle);
-  ctx.lineWidth = 10;
-  ctx.strokeStyle = "#006666";
-  ctx.stroke();
+  var rawSpinnerAngle = (0.25 + 2 * (rawshift * 100 - Math.floor(rawshift * 100))) * Math.PI;
+  var spinnerAngle = (0.25 + 2 * decimals[2] / 10) * Math.PI;
+  var nextSpinnerAngle = spinnerAngle + 0.2 * Math.PI;
 
-  ctx.beginPath();
-  ctx.arc(150, 150, 50, startAngle, subtitheAngle);
-  ctx.lineWidth = 12;
-  ctx.strokeStyle = "#0000ff";
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(150, 150, 25, startAngle, nextSpinnerAngle);
-  ctx.lineWidth = 8;
-  ctx.strokeStyle = "#d3d3d3";
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(150, 150, 25, startAngle, rawSpinnerAngle);
-  ctx.lineWidth = 10;
-  ctx.strokeStyle = "#a0a0a0";
-  ctx.stroke();
-
-  ctx.beginPath();
-  ctx.arc(150, 150, 25, startAngle, spinnerAngle);
-  ctx.lineWidth = 12;
-  ctx.strokeStyle = "#000000";
-  ctx.stroke();
+  drawClockArc(ctx, 25, 8, "#d3d3d3", nextSpinnerAngle);
+  drawClockArc(ctx, 25, 10, "#a0a0a0", rawSpinnerAngle);
+  drawClockArc(ctx, 25, 12, "#000000", spinnerAngle);
 
   var t = setTimeout(getTime, 500);
 }
 
+function drawClockArc(ctx, radius, lineWidth, color, endAngle) {
+  var startAngle = 0.25 * Math.PI;
+  var center_x = 150;
+  var center_y = 150;
+
+  ctx.beginPath();
+  ctx.arc(center_x, center_y, radius, startAngle, endAngle);
+  ctx.lineWidth = lineWidth;
+  ctx.strokeStyle = color;
+  ctx.stroke();
+}
