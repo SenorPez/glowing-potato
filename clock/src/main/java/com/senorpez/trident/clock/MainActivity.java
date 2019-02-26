62d5ee24480ca5292b12412a2d3387674dd6c680
SenorPez <senorpez@senorpez.com>
2019-02-25 23:54:46 -0600

    Calculation of year. Static time.

--- clock/src/main/java/com/senorpez/trident/clock/MainActivity.java
+++ clock/src/main/java/com/senorpez/trident/clock/MainActivity.java
@@ -2,6 +2,11 @@ package com.senorpez.trident.clock;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Date date = new Date();
+        TextView currentTime = this.findViewById(R.id.standardTime);
+        currentTime.setText(DateFormat.getDateTimeInstance().format(date));
+
+        TextView tavenTime = this.findViewById(R.id.tavenTime);
+        tavenTime.setText(String.format(Locale.US, "%d FY", new TavenTime().getYear()));
+
+//        ProgressBar progressBar = (ProgressBar) this.findViewById(R.id.progressBar);
+//        TavenTime tavenTime = new TavenTime();
+//        progressBar.setProgress(tavenTime.getYear());
+//        ObjectAnimator animator = ObjectAnimator.ofInt(progressBar, "progress", 0, 400);
+//        animator.setDuration(5000);
+//        animator.setInterpolator(new DecelerateInterpolator());
     }
+
+
 }
