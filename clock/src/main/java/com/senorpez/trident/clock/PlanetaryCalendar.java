a20290ea80d2992da7566b0721dc2ede4039064f
SenorPez <senorpez@senorpez.com>
2019-02-26 18:42:32 -0600

    Implementation of testGetYear.

--- clock/src/main/java/com/senorpez/trident/clock/PlanetaryCalendar.java
+++ clock/src/main/java/com/senorpez/trident/clock/PlanetaryCalendar.java
@@ -1,7 +1,16 @@
 package com.senorpez.trident.clock;
 
 class PlanetaryCalendar {
-    int getYear(final int days) {
-        return -1;
+    int getYear(int localDays) {
+        int year = 1;
+        while (localDays >= getDaysInYear(year)) {
+            localDays -= getDaysInYear(year);
+            year += 1;
+        }
+        return year;
+    }
+
+    private int getDaysInYear(final int year) {
+        return year % 3 == 0 && year % 51 != 0 ? 100 : 99;
     }
 }
