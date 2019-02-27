7f4168a5de934557976aff4e51793a46a215ceca
SenorPez <senorpez@senorpez.com>
2019-02-26 20:15:19 -0600

    Update test delta for getLocalMilliseconds.

--- clock/src/test/java/com/senorpez/trident/clock/PlanetaryCalendarTest.java
+++ clock/src/test/java/com/senorpez/trident/clock/PlanetaryCalendarTest.java
@@ -35,15 +35,15 @@ public class PlanetaryCalendarTest {
     @Parameterized.Parameters(name = "days: {1}")
     public static Collection params() {
         return Arrays.asList(new Object[][]{
-                {47, 4569, 598104719656.92, 601067070623.45},
-                {48, 4668, 611064309774.24, 614026660740.77},
-                {49, 4768, 624154804842.24, 627117155808.77},
-                {50, 4867, 637114394959.56, 640076745926.09},
-                {51, 4966, 650073985076.88, 653036336043.41},
-                {52, 5065, 663033575194.20, 665995926160.73},
-                {53, 5164, 675993165311.52, 678955516278.05},
-                {54, 5263, 688952755428.84, 691915106395.37},
-                {55, 5363, 702043250496.84, 705005601463.37}
+                {47, 4569, 598104719656.92, 601067070623.448},
+                {48, 4668, 611064309774.24, 614026660740.768},
+                {49, 4768, 624154804842.24, 627117155808.768},
+                {50, 4867, 637114394959.56, 640076745926.088},
+                {51, 4966, 650073985076.88, 653036336043.408},
+                {52, 5065, 663033575194.20, 665995926160.728},
+                {53, 5164, 675993165311.52, 678955516278.048},
+                {54, 5263, 688952755428.84, 691915106395.368},
+                {55, 5363, 702043250496.84, 705005601463.368}
         });
     }
 
@@ -59,6 +59,6 @@ public class PlanetaryCalendarTest {
 
     @Test
     public void testGetLocalMilliseconds() {
-        assertThat(planetaryCalendar.getLocalMilliseconds(standardMilliseconds), closeTo(planetaryMilliseconds, 1e-9));
+        assertThat(planetaryCalendar.getLocalMilliseconds(standardMilliseconds), closeTo(planetaryMilliseconds, 1));
     }
 }
