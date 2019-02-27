9cee02d8afc63b2120a9edda6204fbb81d5cf151
SenorPez <senorpez@senorpez.com>
2019-02-26 23:39:56 -0600

    Update to MainActivity with new class.

--- clock/src/main/java/com/senorpez/trident/clock/MainActivity.java
+++ clock/src/main/java/com/senorpez/trident/clock/MainActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import java.text.DateFormat;
+import java.time.*;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Date date = new Date();
-        TextView currentTime = this.findViewById(R.id.standardTime);
-        currentTime.setText(DateFormat.getDateTimeInstance().format(date));
+        TextView standardTime = this.findViewById(R.id.standardTime);
+        standardTime.setText(DateFormat.getDateTimeInstance().format(date));
+
+        Clock clock = Clock.offset(
+                Clock.systemUTC(),
+                Duration.ofMillis(Clock.fixed(Instant.parse("2000-01-01T00:00:00Z"), ZoneId.ofOffset("GMT", ZoneOffset.UTC)).millis() * -1)
+        );
+        PlanetaryCalendar tavenCalendar = new PlanetaryCalendar(clock);
+        TextView tavenTime = this.findViewById(R.id.tavenTime);
+        tavenTime.setText(String.format(Locale.US, "%d FY", tavenCalendar.getLocalYear()));
     }
 }
