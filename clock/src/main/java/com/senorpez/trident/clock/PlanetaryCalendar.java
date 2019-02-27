58e504adf0e83ace6de0ad308ac9e7be35fd04f8
SenorPez <senorpez@senorpez.com>
2019-02-26 20:06:27 -0600

    Tests for getLocalMilliseconds.

--- clock/src/main/java/com/senorpez/trident/clock/PlanetaryCalendar.java
+++ clock/src/main/java/com/senorpez/trident/clock/PlanetaryCalendar.java
@@ -3,6 +3,7 @@ package com.senorpez.trident.clock;
 class PlanetaryCalendar {
     // TODO: API integration with cache fallback.
     private static final double STD_HOURS_PER_DAY = 36.3624863;
+    private static final double EPOCH_OFFSET = -34.28646952;
 
     int getLocalYear(double localDays) {
         int year = 1;
@@ -20,5 +21,9 @@ class PlanetaryCalendar {
 
     private int getDaysInYear(final int year) {
         return year % 3 == 0 && year % 51 != 0 ? 100 : 99;
+    }
+
+    double getLocalMilliseconds(final double standardMilliseconds) {
+        return -1;
     }
 }
--- clock/src/test/java/com/senorpez/trident/clock/PlanetaryCalendarTest.java
+++ clock/src/test/java/com/senorpez/trident/clock/PlanetaryCalendarTest.java
@@ -16,7 +16,8 @@ import static org.hamcrest.Matchers.closeTo;
 public class PlanetaryCalendarTest {
     private int localYear;
     private int days;
-    private double milliseconds;
+    private double planetaryMilliseconds;
+    private double standardMilliseconds;
     private PlanetaryCalendar planetaryCalendar;
 
     @Before
@@ -24,34 +25,40 @@ public class PlanetaryCalendarTest {
         planetaryCalendar = new PlanetaryCalendar();
     }
 
-    public PlanetaryCalendarTest(int localYear, int days, double milliseconds) {
+    public PlanetaryCalendarTest(int localYear, int days, double planetaryMilliseconds, double standardMilliseconds) {
         this.localYear = localYear;
         this.days = days;
-        this.milliseconds = milliseconds;
+        this.planetaryMilliseconds = planetaryMilliseconds;
+        this.standardMilliseconds = standardMilliseconds;
     }
 
     @Parameterized.Parameters(name = "days: {1}")
     public static Collection params() {
         return Arrays.asList(new Object[][]{
-                {47, 4569, 598104719656.92},
-                {48, 4668, 611064309774.24},
-                {49, 4768, 624154804842.24},
-                {50, 4867, 637114394959.56},
-                {51, 4966, 650073985076.88},
-                {52, 5065, 663033575194.20},
-                {53, 5164, 675993165311.52},
-                {54, 5263, 688952755428.84},
-                {55, 5363, 702043250496.84}
+                {47, 4569, 598104719656.92, 601067070623.45},
+                {48, 4668, 611064309774.24, 614026660740.77},
+                {49, 4768, 624154804842.24, 627117155808.77},
+                {50, 4867, 637114394959.56, 640076745926.09},
+                {51, 4966, 650073985076.88, 653036336043.41},
+                {52, 5065, 663033575194.20, 665995926160.73},
+                {53, 5164, 675993165311.52, 678955516278.05},
+                {54, 5263, 688952755428.84, 691915106395.37},
+                {55, 5363, 702043250496.84, 705005601463.37}
         });
     }
 
     @Test
-    public void testGetYear() {
+    public void testGetLocalYear() {
         assertThat(planetaryCalendar.getLocalYear(days), is(localYear));
     }
 
     @Test
     public void testGetLocalDays() {
-        assertThat(planetaryCalendar.getLocalDays(milliseconds), closeTo(days, 1e-9));
+        assertThat(planetaryCalendar.getLocalDays(planetaryMilliseconds), closeTo(days, 1e-9));
+    }
+
+    @Test
+    public void testGetLocalMilliseconds() {
+        assertThat(planetaryCalendar.getLocalMilliseconds(standardMilliseconds), closeTo(planetaryMilliseconds, 1e-9));
     }
 }
