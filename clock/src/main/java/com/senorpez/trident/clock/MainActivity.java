656c8a997ebfb61b5d1b59b33358124d7031ab4b
SenorPez <senorpez@senorpez.com>
2019-02-27 19:52:54 -0600

    Refactored MainActivity.

--- clock/src/main/java/com/senorpez/trident/clock/MainActivity.java
+++ clock/src/main/java/com/senorpez/trident/clock/MainActivity.java
@@ -10,64 +10,54 @@ import java.time.*;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView standardTime;
+    private TextView tavenTime;
+
+    private ProgressBar progressShift;
+    private ProgressBar progressTithe;
+    private ProgressBar progressSubtithe;
+    private ProgressBar progressTicker;
+
+    private Clock clockJ2000 = Clock.offset(
+            Clock.systemUTC(),
+            Duration.ofMillis(
+                    Clock.fixed(Instant.parse("2000-01-01T00:00:00Z"), ZoneId.ofOffset("GMT", ZoneOffset.UTC)).millis() * -1));
+    private PlanetaryCalendar tavenCalendar = new PlanetaryCalendar(clockJ2000);
+
+    private Handler handler = new Handler();
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            standardTime.setText(String.format("%s", Clock.systemUTC().instant()));
+            tavenTime.setText(String.format(
+                    Locale.US,
+                    "%d FY %d Caste %d Day %d.%02d Shift",
+                    tavenCalendar.getLocalYear(),
+                    tavenCalendar.getCaste(),
+                    tavenCalendar.getCasteDay(),
+                    tavenCalendar.getShift(),
+                    (int) Math.floor(tavenCalendar.getTithe() * 100)));
+            progressShift.setProgress(tavenCalendar.getShift() - 1);
+            progressTithe.setProgress((int) Math.floor(tavenCalendar.getTithe() * 10));
+            progressSubtithe.setProgress((int) (Math.floor(tavenCalendar.getTithe() * 100)) % 10);
+            progressTicker.setProgress((int) (Math.floor(tavenCalendar.getTithe() * 1000)) % 10);
+            handler.postDelayed(this, 500);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView standardTime = this.findViewById(R.id.standardTime);
-        standardTime.setText(String.format("%s", Clock.systemUTC().instant()));
+        standardTime = this.findViewById(R.id.standardTime);
+        tavenTime = this.findViewById(R.id.tavenTime);
 
-        Clock clock = Clock.offset(
-                Clock.systemUTC(),
-                Duration.ofMillis(
-                        Clock.fixed(Instant.parse("2000-01-01T00:00:00Z"), ZoneId.ofOffset("GMT", ZoneOffset.UTC)).millis() * -1));
+        progressShift = this.findViewById(R.id.prgShift);
+        progressTithe = this.findViewById(R.id.prgTithe);
+        progressSubtithe = this.findViewById(R.id.prgSubtithe);
+        progressTicker = this.findViewById(R.id.prgTicker);
 
-        PlanetaryCalendar tavenCalendar = new PlanetaryCalendar(clock);
-        TextView tavenTime = this.findViewById(R.id.tavenTime);
-
-        tavenTime.setText(String.format(
-                Locale.US,
-                "%d FY %d Caste %d Day %d.%d Shift",
-                tavenCalendar.getLocalYear(),
-                tavenCalendar.getCaste(),
-                tavenCalendar.getCasteDay(),
-                tavenCalendar.getShift(),
-                (int) Math.floor(tavenCalendar.getTithe() * 100)));
-
-        ProgressBar progressShift = this.findViewById(R.id.prgShift);
-        progressShift.setProgress(tavenCalendar.getShift() - 1);
-
-        ProgressBar progressTithe = this.findViewById(R.id.prgTithe);
-        progressTithe.setProgress((int) Math.floor(tavenCalendar.getTithe() * 10));
-
-        ProgressBar progressSubtithe = this.findViewById(R.id.prgSubtithe);
-        progressSubtithe.setProgress((int) (Math.floor(tavenCalendar.getTithe() * 100)) % 10);
-
-        ProgressBar progressTicker = this.findViewById(R.id.prgTicker);
-        progressTicker.setProgress((int) (Math.floor(tavenCalendar.getTithe() * 1000)) % 10);
-
-        Handler handler = new Handler();
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                standardTime.setText(String.format("%s", Clock.systemUTC().instant()));
-                tavenTime.setText(String.format(
-                        Locale.US,
-                        "%d FY %d Caste %d Day %d.%d Shift",
-                        tavenCalendar.getLocalYear(),
-                        tavenCalendar.getCaste(),
-                        tavenCalendar.getCasteDay(),
-                        tavenCalendar.getShift(),
-                        (int) Math.floor(tavenCalendar.getTithe() * 100)));
-                handler.postDelayed(this, 500);
-                progressShift.setProgress(tavenCalendar.getShift() - 1);
-                progressTithe.setProgress((int) Math.floor(tavenCalendar.getTithe() * 10));
-                progressSubtithe.setProgress((int) (Math.floor(tavenCalendar.getTithe() * 100)) % 10);
-                progressTicker.setProgress((int) (Math.floor(tavenCalendar.getTithe() * 1000)) % 10);
-            }
-        };
         handler.post(runnable);
     }
 }
