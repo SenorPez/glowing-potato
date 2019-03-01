c5ec50fa041a7736c2e845d1343028bd4aca6d67
SenorPez <senorpez@senorpez.com>
2019-02-28 23:02:41 -0600

    Implementation of getPlanetaryCalendar.

--- clock/src/main/java/com/senorpez/trident/clock/PlanetaryCalendarViewModel.java
+++ clock/src/main/java/com/senorpez/trident/clock/PlanetaryCalendarViewModel.java
@@ -3,10 +3,13 @@ package com.senorpez.trident.clock;
 import android.arch.lifecycle.ViewModel;
 
 public class PlanetaryCalendarViewModel extends ViewModel {
+    private PlanetaryCalendar planetaryCalendar;
+
     public PlanetaryCalendarViewModel(PlanetaryCalendar planetaryCalendar) {
+        this.planetaryCalendar = planetaryCalendar;
     }
 
     PlanetaryCalendar getPlanetaryCalendar() {
-        return null;
+        return planetaryCalendar;
     }
 }
