004f34b34619c8fdd67c9979551c6c542d2a3cdd
SenorPez <senorpez@senorpez.com>
2019-03-02 12:14:26 -0600

    Tests for getLocalDateTime for Midfestival Day.

--- clock/src/test/java/com/senorpez/trident/clock/PlanetaryCalendarViewModelTest.java
+++ clock/src/test/java/com/senorpez/trident/clock/PlanetaryCalendarViewModelTest.java
@@ -64,4 +64,14 @@ public class PlanetaryCalendarViewModelTest {
         when(planetaryCalendar.getTithe()).thenReturn(0.329);
         assertThat(planetaryCalendarViewModel.getLocalDateTime(), is("35 FY Festival Day 3.32 Shift"));
     }
+
+    @Test
+    public void testGetLocalDateTime_MidfestivalDay() {
+        when(planetaryCalendar.getLocalYear()).thenReturn(35);
+        when(planetaryCalendar.getCaste()).thenReturn(3);
+        when(planetaryCalendar.getCasteDay()).thenReturn(0);
+        when(planetaryCalendar.getShift()).thenReturn(3);
+        when(planetaryCalendar.getTithe()).thenReturn(0.329);
+        assertThat(planetaryCalendarViewModel.getLocalDateTime(), is("35 FY Midfestival Day 3.32 Shift"));
+    }
 }
