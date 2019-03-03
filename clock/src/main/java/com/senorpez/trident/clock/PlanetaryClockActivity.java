4c08abe6966d65cd4614cb3492c7792e2b570bf4
SenorPez <senorpez@senorpez.com>
2019-03-02 21:59:13 -0600

    Added observers for LiveData elements.

--- clock/src/main/java/com/senorpez/trident/clock/PlanetaryClockActivity.java
+++ clock/src/main/java/com/senorpez/trident/clock/PlanetaryClockActivity.java
@@ -1,6 +1,8 @@
 package com.senorpez.trident.clock;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
@@ -20,5 +22,30 @@ public class PlanetaryClockActivity extends AppCompatActivity {
                 Duration.ofMillis(
                         Clock.fixed(Instant.parse("2000-01-01T00:00:00Z"), ZoneId.ofOffset("GMT", ZoneOffset.UTC)).millis() * -1));
         planetaryCalendarViewModel.init(new PlanetaryCalendar(clockJ2000));
+
+        planetaryCalendarViewModel.getShift().observe(this, shift -> {
+            ProgressBar progressShift = this.findViewById(R.id.prgShift);
+            progressShift.setProgress(shift - 1);
+        });
+        planetaryCalendarViewModel.getTithe().observe(this, tithe -> {
+            ProgressBar progressTithe = this.findViewById(R.id.prgTithe);
+            progressTithe.setProgress(tithe);
+        });
+        planetaryCalendarViewModel.getSubtithe().observe(this, subTithe -> {
+            ProgressBar progressSubTithe = this.findViewById(R.id.prgSubtithe);
+            progressSubTithe.setProgress(subTithe);
+        });
+        planetaryCalendarViewModel.getSpinner().observe(this, spinner -> {
+            ProgressBar progressSpinner = this.findViewById(R.id.prgTicker);
+            progressSpinner.setProgress(spinner);
+        });
+        planetaryCalendarViewModel.getLocalDateTime().observe(this, localDateTime -> {
+            TextView textLocalDateTime = this.findViewById(R.id.tavenTime);
+            textLocalDateTime.setText(localDateTime);
+        });
+        planetaryCalendarViewModel.getStandardDateTime().observe(this, standardDateTime -> {
+            TextView textStandardDateTime = this.findViewById(R.id.standardTime);
+            textStandardDateTime.setText(standardDateTime);
+        });
     }
 }
