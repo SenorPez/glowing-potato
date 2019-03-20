d05ee02246e7502e17a7319e2313de41ca05072a
SenorPez <senorpez@senorpez.com>
2019-03-20 19:15:31 +0000

    Refactored tests and implemenation to walk through API.

--- tridentweb/test/test_Constant.py
+++ tridentweb/test/test_Constant.py
@@ -8,65 +8,90 @@ from unittest.mock import sentinel
 
 from tridentweb.Constant import Constant
 
-class TestConstant(unittest.TestCase):
-    """Unit tests against the Constant object."""
-    def mocked_requests_get(*args, **kwargs):
-        """Defines a response suitable for mocking requests responses."""
-        class MockResponse:
-            """Solution cribbed from
-            https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response/28507806#28507806
-            """
-            def __init__(self, *, name="", value=0, units="", symbol=""):
+def mocked_requests_get(*args, **kwargs):
+    """Defines a response suitable for mocking requests responses."""
+    class MockResponse:
+        """Solution cribbed from
+        https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response/28507806#28507806
+        """
+        def __init__(self, *, json_string=None, name="", value=0, units="", symbol=""):
+            if json_string is None:
                 json_string = ("{{"
                                "\"name\": \"{0}\","
                                "\"value\": {1},"
                                "\"units\": \"{2}\","
                                "\"symbol\": \"{3}\"}}").format(name, value, units, symbol)
-                self.json_data = json.loads(json_string)
+            self.json_data = json.loads(json_string)
 
-            def json(self):
-                """Returns json data of response."""
-                return self.json_data
+        def json(self):
+            """Returns json data of response."""
+            return self.json_data
 
-            def raise_for_status(self):
-                """Return raise for status of response."""
-                return None
+        @staticmethod
+        def raise_for_status():
+            """Return raise for status of response."""
+            return None
 
-        yield MockResponse(*args, **kwargs)
+    return MockResponse(*args, **kwargs)
 
+class TestConstant(unittest.TestCase):
+    """Unit tests against the Constant object."""
+    api_traversal = [
+        mocked_requests_get(json_string=( \
+            "{\"_links\": {\"trident-api:constants\":"
+            "{\"href\": \"http://trident.senorpez.com/constants\"}}}")),
+        mocked_requests_get(json_string=( \
+            "{\"_embedded\": {\"trident-api:constant\":"
+            "[{\"symbol\": \"MC\","
+            "\"_links\": {\"self\":"
+            "{\"href\": \"http://trident.senorpez.com/constants/MC\"}}}]}}"))]
 
-    @mock.patch('requests.get', side_effect=mocked_requests_get())
-    def test_init(self, _):
+    @mock.patch('requests.get')
+    def test_init(self, mock_get):
         """Test Constant init."""
+        mock_get.side_effect = self.api_traversal \
+            + [mocked_requests_get()]
+
         instance = Constant("MC")
         expected_result = Constant
         self.assertIsInstance(instance, expected_result)
 
-
-    @mock.patch('requests.get', side_effect=mocked_requests_get(name=id(sentinel.name)))
-    def test_property_name(self, _):
+    @mock.patch('requests.get')
+    def test_property_name(self, mock_get):
         """Test name property of Constant."""
+        mock_get.side_effect = self.api_traversal \
+            + [mocked_requests_get(name=id(sentinel.name))]
+
         instance = Constant("MC")
         expected_result = str(id(sentinel.name))
         self.assertEqual(instance.name, expected_result)
 
-    @mock.patch('requests.get', side_effect=mocked_requests_get(value=id(sentinel.value)))
-    def test_property_value(self, _):
+    @mock.patch('requests.get')
+    def test_property_value(self, mock_get):
         """Test value property of Constant."""
+        mock_get.side_effect = self.api_traversal \
+            + [mocked_requests_get(value=id(sentinel.value))]
+
         instance = Constant("MC")
         expected_result = id(sentinel.value)
         self.assertEqual(instance.value, expected_result)
 
-    @mock.patch('requests.get', side_effect=mocked_requests_get(units=id(sentinel.units)))
-    def test_property_units(self, _):
+    @mock.patch('requests.get')
+    def test_property_units(self, mock_get):
         """Test units property of Constant."""
+        mock_get.side_effect = self.api_traversal \
+            + [mocked_requests_get(units=id(sentinel.units))]
+
         instance = Constant("MC")
         expected_result = str(id(sentinel.units))
         self.assertEqual(instance.units, expected_result)
 
-    @mock.patch('requests.get', side_effect=mocked_requests_get(symbol=id(sentinel.symbol)))
-    def test_property_symbol(self, _):
+    @mock.patch('requests.get')
+    def test_property_symbol(self, mock_get):
         """Test symbol property of Constant."""
+        mock_get.side_effect = self.api_traversal \
+            + [mocked_requests_get(symbol=id(sentinel.symbol))]
+
         instance = Constant("MC")
         expected_result = str(id(sentinel.symbol))
         self.assertEqual(instance.symbol, expected_result)
--- tridentweb/tridentweb/Constant.py
+++ tridentweb/tridentweb/Constant.py
@@ -10,8 +10,19 @@ class Constant:
     constant_symbol - Symbol denoting the constant.
     server_url - Trident API server URL. Defaults to http://trident.senorpez.com/constants/
     """
-    def __init__(self, constant_symbol, server_url="http://trident.senorpez.com/constants/"):
-        req = requests.get(server_url + str(constant_symbol))
+    def __init__(self, constant_symbol, server_url="http://trident.senorpez.com/"):
+        req = requests.get(server_url)
+        req.raise_for_status()
+        constants_url = req.json()['_links']['trident-api:constants']
+
+        req = requests.get(constants_url)
+        req.raise_for_status()
+        constant_url = None
+        for entry in req.json()['_embedded']['trident-api:constant']:
+            if entry['symbol'] == constant_symbol:
+                constant_url = entry['_links']['self']['href']
+
+        req = requests.get(constant_url)
         req.raise_for_status()
 
         self.name = req.json()['name']
