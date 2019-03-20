ffa506d02753c689eb4b6fecd4b1d964a5d9cd41
SenorPez <senorpez@senorpez.com>
2019-03-20 18:05:27 +0000

    Tests for value property of Constant.

--- tridentweb/test/test_Constant.py
+++ tridentweb/test/test_Constant.py
@@ -2,44 +2,53 @@
 
 """
 import json
-import sys
 import unittest
 from unittest import mock
 from unittest.mock import sentinel
 
-from requests.exceptions import HTTPError
 from tridentweb.Constant import Constant
 
 class TestConstant(unittest.TestCase):
     """Unit tests against the Constant object."""
     def mocked_requests_get(*args, **kwargs):
+        """Defines a response suitable for mocking requests responses."""
         class MockResponse:
             """Solution cribbed from
             https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response/28507806#28507806
             """
-            def __init__(self, name=""):
-                self._name = "test"
-                json_string = "{{\"name\": \"{0}\"}}".format(id(sentinel.name))
+            def __init__(self, *, name="", value=0):
+                json_string = "{{\"name\": \"{0}\", \"value\": {1}}}".format(name, value)
                 self.json_data = json.loads(json_string)
 
             def json(self):
+                """Returns json data of response."""
                 return self.json_data
 
             def raise_for_status(self):
+                """Return raise for status of response."""
                 return None
 
-        yield MockResponse(*args)
+        yield MockResponse(*args, **kwargs)
 
 
     @mock.patch('requests.get', side_effect=mocked_requests_get())
-    def test_init(self, mock_get):
+    def test_init(self, _):
+        """Test Constant init."""
         instance = Constant("MC")
         expected_result = Constant
         self.assertIsInstance(instance, expected_result)
 
 
-    @mock.patch('requests.get', side_effect=mocked_requests_get(sentinel.name))
-    def test_property_name(self, mock_get):
+    @mock.patch('requests.get', side_effect=mocked_requests_get(name=id(sentinel.name)))
+    def test_property_name(self, _):
+        """Test name property of Constant."""
         instance = Constant("MC")
         expected_result = str(id(sentinel.name))
         self.assertEqual(instance.name, expected_result)
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get(value=id(sentinel.value)))
+    def test_property_value(self, _):
+        """Test value property of Constant."""
+        instance = Constant("MC")
+        expected_result = id(sentinel.value)
+        self.assertEqual(instance.value, expected_result)
