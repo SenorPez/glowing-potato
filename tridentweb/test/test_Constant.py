b99b56c900563ce527ddfc1dbe5a688d98a19237
SenorPez <senorpez@senorpez.com>
2019-03-20 18:12:31 +0000

    Tests for units property of Constant.

--- tridentweb/test/test_Constant.py
+++ tridentweb/test/test_Constant.py
@@ -16,8 +16,11 @@ class TestConstant(unittest.TestCase):
             """Solution cribbed from
             https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response/28507806#28507806
             """
-            def __init__(self, *, name="", value=0):
-                json_string = "{{\"name\": \"{0}\", \"value\": {1}}}".format(name, value)
+            def __init__(self, *, name="", value=0, units=""):
+                json_string = ("{{"
+                               "\"name\": \"{0}\","
+                               "\"value\": {1},"
+                               "\"units\": \"{2}\"}}").format(name, value, units)
                 self.json_data = json.loads(json_string)
 
             def json(self):
@@ -52,3 +55,10 @@ class TestConstant(unittest.TestCase):
         instance = Constant("MC")
         expected_result = id(sentinel.value)
         self.assertEqual(instance.value, expected_result)
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get(units=id(sentinel.units)))
+    def test_property_units(self, _):
+        """Test units property of Constant."""
+        instance = Constant("MC")
+        expected_result = str(id(sentinel.units))
+        self.assertEqual(instance.units, expected_result)
