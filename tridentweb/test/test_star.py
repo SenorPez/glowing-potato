436766f1d524bef67747a35f3f39f99c27a22a09
SenorPez <senorpez@senorpez.com>
2019-03-21 15:43:10 +0000

    Test for id property of Star.

--- tridentweb/test/test_star.py
+++ tridentweb/test/test_star.py
@@ -14,9 +14,9 @@ def mocked_requests_get(*args, **kwargs):
         """Solution cribbed from
         https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response/28507806#28507806
         """
-        def __init__(self, *, json_string=None):
+        def __init__(self, *, json_string=None, idnum=0):
             if json_string is None:
-                json_string = ("{}")
+                json_string = ("{{\"id\": {0}}}").format(idnum)
             self.json_data = json.loads(json_string)
 
         def json(self):
@@ -56,7 +56,7 @@ class TestStar(unittest.TestCase):
         mock_get.side_effect = self.api_traversal \
                 + [mocked_requests_get()]
 
-        instance = Star(1, 1, 1)
+        instance = Star(1, 1)
         expected_result = Star
         self.assertIsInstance(instance, expected_result)
 
@@ -65,67 +65,77 @@ class TestStar(unittest.TestCase):
         """Test Star init with HTTPError on API index."""
         mock_get.side_effect = HTTPError("Error", None)
         with self.assertRaises(HTTPError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
 
     @mock.patch('requests.get')
     def test_init_index_KeyError(self, mock_get):
         """Test Star init with KeyError on API index."""
         mock_get.side_effect = KeyError()
         with self.assertRaises(KeyError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
 
     @mock.patch('requests.get')
     def test_init_systems_HTTPError(self, mock_get):
         """Test Star init with HTTPError on API systems."""
         mock_get.side_effect = self.api_traversal[0:1] + [HTTPError("Error", None)]
         with self.assertRaises(HTTPError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
 
     @mock.patch('requests.get')
     def test_init_systems_KeyError(self, mock_get):
         """Test Star init with KeyError on API systems."""
         mock_get.side_effect = self.api_traversal[0:1] + [KeyError()]
         with self.assertRaises(KeyError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
 
     @mock.patch('requests.get')
     def test_init_system_HTTPError(self, mock_get):
         """Test Star init with HTTPError on API system."""
         mock_get.side_effect = self.api_traversal[0:2] + [HTTPError("Error", None)]
         with self.assertRaises(HTTPError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
 
     @mock.patch('requests.get')
     def test_init_system_KeyError(self, mock_get):
         """Test Star init with KeyError on API system."""
         mock_get.side_effect = self.api_traversal[0:2] + [KeyError()]
         with self.assertRaises(KeyError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
 
     @mock.patch('requests.get')
     def test_init_stars_HTTPError(self, mock_get):
         """Test Star init with HTTPError on API stars."""
         mock_get.side_effect = self.api_traversal[0:3] + [HTTPError("Error", None)]
         with self.assertRaises(HTTPError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
 
     @mock.patch('requests.get')
     def test_init_stars_KeyError(self, mock_get):
         """Test Star init with KeyError on API stars."""
         mock_get.side_effect = self.api_traversal[0:3] + [KeyError()]
         with self.assertRaises(KeyError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
 
     @mock.patch('requests.get')
     def test_init_star_HTTPError(self, mock_get):
         """Test Star init with HTTPError on API star."""
         mock_get.side_effect = self.api_traversal + [HTTPError("Error", None)]
         with self.assertRaises(HTTPError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
 
     @mock.patch('requests.get')
     def test_init_star_KeyError(self, mock_get):
         """Test Star init with KeyError on API star."""
         mock_get.side_effect = self.api_traversal + [KeyError()]
         with self.assertRaises(KeyError):
-            _ = Star(1, 1, 1)
+            _ = Star(1, 1)
+
+    @mock.patch('requests.get')
+    def test_property_id(self, mock_get):
+        """Test id property of Star."""
+        mock_get.side_effect = self.api_traversal \
+                + [mocked_requests_get(idnum=1)]
+
+        instance = Star(1, 1)
+        expected_result = 1
+        self.assertEqual(instance.id, expected_result)
