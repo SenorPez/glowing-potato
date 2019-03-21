ceeeaf9132e6f8a76ca72ebf374f87dff3b17a93
SenorPez <senorpez@senorpez.com>
2019-03-21 15:35:11 +0000

    Test for init of Star object.

---
+++ tridentweb/test/test_star.py
@@ -0,0 +1,60 @@
+"""Tests star.py
+
+"""
+import json
+import unittest
+from unittest import mock
+
+from tridentweb.star import Star
+
+def mocked_requests_get(*args, **kwargs):
+    """Defines a response suitable for mocking requests responses."""
+    class MockResponse:
+        """Solution cribbed from
+        https://stackoverflow.com/questions/15753390/how-can-i-mock-requests-and-the-response/28507806#28507806
+        """
+        def __init__(self, *, json_string=None):
+            if json_string is None:
+                json_string = ("{}")
+            self.json_data = json.loads(json_string)
+
+        def json(self):
+            """Returns json data of response."""
+            return self.json_data
+
+        @staticmethod
+        def raise_for_status():
+            """Return raise for status of response."""
+            return None
+
+    return MockResponse(*args, **kwargs)
+
+class TestStar(unittest.TestCase):
+    """Unit tests against the Star object."""
+    api_traversal = [
+        mocked_requests_get(json_string=( \
+            "{\"_links\": {\"trident-api:systems\":"
+            "{\"href\": \"http://trident.senorpez.com/systems\"}}}")),
+        mocked_requests_get(json_string=( \
+            "{\"_embedded\": {\"trident-api:system\":"
+            "[{\"id\": 1,"
+            "\"_links\": {\"self\":"
+            "{\"href\": \"http://trident.senorpez.com/systems/1\"}}}]}}")),
+        mocked_requests_get(json_string=( \
+            "{\"_links\": {\"trident-api:stars\":"
+            "{\"href\": \"http://trident.senorpez.com/stars\"}}}")),
+        mocked_requests_get(json_string=( \
+            "{\"_embedded\": {\"trident-api:star\":"
+            "[{\"id\": 1,"
+            "\"_links\": {\"self\":"
+            "{\"href\": \"http://trident.senorpez.com/stars/1\"}}}]}}"))]
+
+    @mock.patch('requests.get')
+    def test_init(self, mock_get):
+        """Test Star init."""
+        mock_get.side_effect = self.api_traversal \
+                + [mocked_requests_get()]
+
+        instance = Star(1, 1, 1)
+        expected_result = Star
+        self.assertIsInstance(instance, expected_result)
---
+++ tridentweb/tridentweb/star.py
@@ -0,0 +1,15 @@
+"""Provides a Star class for using stars from the Trident API.
+
+"""
+import requests
+
+class Star:
+    """Represents a planet.
+
+    Arguments:
+    system_id - ID number denoting the solar system.
+    star_id - ID number denoting the star.
+    server_url - Trident API server URL. Defaults to http://trident.senorpez.com/
+    """
+    def __init__(self, system_id, star_id, server_url="http://trident.senorpez.com/"):
+        pass
