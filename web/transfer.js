a86734c274242f9139d4f5e518841c804b1869a4
SenorPez <senorpez@senorpez.com>
2019-06-21 19:49:30 +0000

    Change to use new function name.

--- web/transfer.js
+++ web/transfer.js
@@ -6,7 +6,7 @@ var x_max = 500;
 
 $(document).ready(function() {
   var posting = $.post(
-    "http://senorpez.com:5001/transfer2",
+    "http://senorpez.com:5001/transfer",
     {
       flight_start: y_min,
       flight_end: y_max,
