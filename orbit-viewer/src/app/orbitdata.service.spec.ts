1dde841681a758589df18ddf0ae2aa232fe9a162
SenorPez <senorpez@senorpez.com>
2021-08-18 18:01:26 -0500

    Test orbit period

--- orbit-viewer/src/app/orbitdata.service.spec.ts
+++ orbit-viewer/src/app/orbitdata.service.spec.ts
@@ -34,7 +34,7 @@ describe('OrbitdataService unit tests', () => {
     })
   });
 
-  describe("ephermerides", () => {
+  describe("ephemerides", () => {
     const dummyLink: Link = {
       href: "/"
     };
@@ -136,5 +136,125 @@ describe('OrbitdataService unit tests', () => {
         } else expect(velocityResult.z).toEqual(0);
       });
     });
-  })
+  });
+
+  describe("orbitPeriod", () => {
+    const dummyLink: Link = {
+      href: "/"
+    };
+
+    const omegaHydri1: Planet = {
+      id: 2035226060,
+      name: "1 Omega Hydri 1",
+      mass: 0.027045919,
+      radius: 0.33969113,
+      semimajorAxis: 0.30473167,
+      eccentricity: 0.115,
+      inclination: 0.001120502,
+      longitudeOfAscendingNode: 3.826163,
+      argumentOfPeriapsis: 1.9260389,
+      trueAnomalyAtEpoch: 4.129414,
+
+      GM: 10780216782443.096,
+      starGM: 1.3867924002240001e+20,
+      lagrangePoints: {L1: false, L2: false, L3: false, L4: false, L5: false},
+
+      _links: {
+        curies: [],
+        index: dummyLink,
+        self: dummyLink,
+        "trident-api:calendars": dummyLink,
+        "trident-api:planets": dummyLink
+      }
+    }
+
+    const omegaHydri2: Planet = {
+      id: -154475081,
+      name: "1 Omega Hydri 2",
+      mass: 0.2618094,
+      radius: 0.7204918,
+      semimajorAxis: 0.54851705,
+      eccentricity: 0.03,
+      inclination: 0.014861423,
+      longitudeOfAscendingNode: 4.87442,
+      argumentOfPeriapsis: 3.2554474,
+      trueAnomalyAtEpoch: 1.2192137,
+
+      GM: 283935784992609,
+      starGM: 1.3867924002240001e+20,
+      lagrangePoints: {L1: false, L2: false, L3: false, L4: false, L5: false},
+
+      _links: {
+        curies: [],
+        index: dummyLink,
+        self: dummyLink,
+        "trident-api:calendars": dummyLink,
+        "trident-api:planets": dummyLink
+      }
+    }
+
+    const omegaHydri3: Planet = {
+      id: 159569841,
+      name: "1 Omega Hydri 3",
+      mass: 0.71235156,
+      radius: 0.9022098,
+      semimajorAxis: 0.93247896,
+      eccentricity: 0.115,
+      inclination: 0,
+      longitudeOfAscendingNode: 0,
+      argumentOfPeriapsis: 3.1809726,
+      trueAnomalyAtEpoch: 4.653676,
+
+      GM: 283935784992609,
+      starGM: 1.3867924002240001e+20,
+      lagrangePoints: {L1: false, L2: false, L3: false, L4: false, L5: false},
+
+      _links: {
+        curies: [],
+        index: dummyLink,
+        self: dummyLink,
+        "trident-api:calendars": dummyLink,
+        "trident-api:planets": dummyLink
+      }
+    }
+
+    const omegaHydri4: Planet = {
+      id: 1008667220,
+      name: "1 Omega Hydri 4",
+      mass: 0.06909913,
+      radius: 0.4282164,
+      semimajorAxis: 1.5852143,
+      eccentricity: 0.05,
+      inclination: 0.032868735,
+      longitudeOfAscendingNode: 3.5879867,
+      argumentOfPeriapsis: 0.6304199,
+      trueAnomalyAtEpoch: 2.9054375,
+
+      GM: 27542181165232.988,
+      starGM: 1.3867924002240001e+20,
+      lagrangePoints: {L1: false, L2: false, L3: false, L4: false, L5: false},
+
+      _links: {
+        curies: [],
+        index: dummyLink,
+        self: dummyLink,
+        "trident-api:calendars": dummyLink,
+        "trident-api:planets": dummyLink
+      }
+    }
+
+    const parameters = [
+      {description: "1 Omega Hydri 1 at 5193242.83", planet: omegaHydri1, time: 5193242.83},
+      {description: "1 Omega Hydri 2 at 12541428.68", planet: omegaHydri2, time: 12541428.68},
+      {description: "1 Omega Hydri 3 at 27798436.97", planet: omegaHydri3, time: 27798436.97},
+      {description: "1 Omega Hydri 4 at 61616097.55", planet: omegaHydri4, time: 61616097.55},
+    ];
+
+    parameters.forEach(parameter => {
+      it(parameter.description, () => {
+        const period: number = service.orbitalPeriod(parameter.planet);
+        expect(period).toBeCloseTo(parameter.time, 2);
+      });
+    });
+  });
 });
