fdbeca2ad91aa9f1ce631502e52c2f34a35eb254
SenorPez <senorpez@senorpez.com>
2021-08-18 11:10:13 -0500

    Test lagrangeStability

--- orbit-viewer/src/app/orbitdata.service.spec.ts
+++ orbit-viewer/src/app/orbitdata.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed } from '@angular/core/testing';
 
 import { OrbitdataService } from './orbitdata.service';
 
-describe('OrbitdataService', () => {
+describe('OrbitdataService unit tests', () => {
   let service: OrbitdataService;
 
   beforeEach(() => {
@@ -10,7 +10,25 @@ describe('OrbitdataService', () => {
     service = TestBed.inject(OrbitdataService);
   });
 
-  it('should be created', () => {
+  it('service should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe("lagrangeStability", () => {
+    const parameters = [
+      {description: "Should pass with solar mass 100 and planet mass 1", solarMass: 100, planetMass: 1, result: true},
+      {description: "Should pass with solar mass 50 and planet mass 1", solarMass: 50, planetMass: 1, result: true},
+      {description: "Should pass with solar mass 25 and planet mass 1", solarMass: 25, planetMass: 1, result: true},
+      {description: "Should fail with solar mass ~24.959 and planet mass 1", solarMass: (25 + Math.sqrt(621)) / 2, planetMass: 1, result: false},
+      {description: "Should fail with solar mass 10 and planet mass 1", solarMass: 10, planetMass: 1, result: false},
+      {description: "Should fail with solar mass 1 and planet mass 1", solarMass: 1, planetMass: 1, result: false}
+    ];
+
+    parameters.forEach(parameter => {
+      it(parameter.description, () => {
+        const result = service.lagrangeStability(parameter.solarMass, parameter.planetMass);
+        expect(result).toBe(parameter.result);
+      })
+    })
   });
 });
