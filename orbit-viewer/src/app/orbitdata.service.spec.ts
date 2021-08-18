0699d6a27a4c36d11429fa4ef8c85fbc37767d66
SenorPez <senorpez@senorpez.com>
2021-08-18 13:36:08 -0500

    Test ephemerides

--- orbit-viewer/src/app/orbitdata.service.spec.ts
+++ orbit-viewer/src/app/orbitdata.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { OrbitdataService } from './orbitdata.service';
+import {Link, Planet} from "./api.service";
+import {Vector3} from "three";
 
 describe('OrbitdataService unit tests', () => {
   let service: OrbitdataService;
@@ -31,4 +33,108 @@ describe('OrbitdataService unit tests', () => {
       })
     })
   });
+
+  describe("ephermerides", () => {
+    const dummyLink: Link = {
+      href: "/"
+    };
+
+    const omegaHydri3: Planet = {
+      id: 159569841,
+      name: "1 Omega Hydri 3",
+      mass: 0.71235156,
+      radius: 0.9022098,
+      semimajorAxis: 0.93247896,
+      eccentricity: 0.115,
+      inclination: 0,
+      longitudeOfAscendingNode: 0,
+      argumentOfPeriapsis: 3.1809726,
+      trueAnomalyAtEpoch: 4.653676,
+
+      GM: 283935784992609,
+      starGM: 1.3867924002240001e+20,
+      lagrangePoints: {L1: false, L2: false, L3: false, L4: false, L5: false},
+
+      _links: {
+        curies: [],
+        index: dummyLink,
+        self: dummyLink,
+        "trident-api:calendars": dummyLink,
+        "trident-api:planets": dummyLink
+      }
+    }
+
+    const omegaHydri4: Planet = {
+      id: 1008667220,
+      name: "1 Omega Hydri 4",
+      mass: 0.06909913,
+      radius: 0.4282164,
+      semimajorAxis: 1.5852143,
+      eccentricity: 0.05,
+      inclination: 0.032868735,
+      longitudeOfAscendingNode: 3.5879867,
+      argumentOfPeriapsis: 0.6304199,
+      trueAnomalyAtEpoch: 2.9054375,
+
+      GM: 27542181165232.988,
+      starGM: 1.3867924002240001e+20,
+      lagrangePoints: {L1: false, L2: false, L3: false, L4: false, L5: false},
+
+      _links: {
+        curies: [],
+        index: dummyLink,
+        self: dummyLink,
+        "trident-api:calendars": dummyLink,
+        "trident-api:planets": dummyLink
+      }
+    }
+
+    const parameters = [
+      {description: "1 Omega Hydri 3 at epoch -100", planet: omegaHydri3, time: -8640000, position: new Vector3(153962795195.52725, -19978155934.90074, 0.0), velocity: new Vector3(4228.10592990422, 27829.32663385655, 0.0)},
+      {description: "1 Omega Hydri 3 at epoch 0", planet: omegaHydri3, time: 0, position: new Vector3(2679144592.007491, 138561324414.57434, 0.0), velocity: new Vector3(-31590.91107946407, -3033.730289528269, 0.0)},
+      {description: "1 Omega Hydri 3 at epoch 100", planet: omegaHydri3, time: 8640000, position: new Vector3(-94204526387.16089, -84343661283.09836, 0.0), velocity: new Vector3(21315.863343264646, -27294.78951134675, 0.0)},
+      {description: "1 Omega Hydri 4 at epoch -100", planet: omegaHydri4, time: -8640000, position: new Vector3(242462782731.64142, 5829281881.332565, 3268886044.652998), velocity: new Vector3(493.7639685634058, 23621.49948081852, -693.5715821677534)},
+      {description: "1 Omega Hydri 4 at epoch 0", planet: omegaHydri4, time: 0, position: new Vector3(165857354562.44604, 185209860374.5007, -3138709539.4643126), velocity: new Vector3(-16969.547861766354, 15564.752155681881, -702.5125448035453)},
+      {description: "1 Omega Hydri 4 at epoch 100", planet: omegaHydri4, time: 8640000, position: new Vector3(-17547407271.258133, 246239664608.57016, -7552210137.703378), velocity: new Vector3(-23083.357177718353, -2303.364648217048, -259.3551307763122)},
+    ];
+
+    parameters.forEach(parameter => {
+      it(parameter.description, () => {
+        const [positionResult, velocityResult]: [Vector3, Vector3] = service.ephemerides(parameter.planet, parameter.time);
+
+        // Tolerance based on percentage difference.
+        const tolerance = 1e-8;
+
+        if (parameter.position.x !== 0) {
+          expect(tolerance - Math.abs(positionResult.x / parameter.position.x - 1))
+            .toBeGreaterThanOrEqual(0, [positionResult.x, parameter.position.x]);
+        } else expect(positionResult.x).toEqual(0);
+
+        if (parameter.position.y !== 0) {
+          expect(tolerance - Math.abs(positionResult.y / parameter.position.y - 1))
+            .toBeGreaterThanOrEqual(0, [positionResult.y, parameter.position.y]);
+        } else expect(positionResult.y).toEqual(0);
+
+        if (parameter.position.z !== 0) {
+          expect(tolerance - Math.abs(positionResult.z / parameter.position.z - 1))
+            .toBeGreaterThanOrEqual(0, [positionResult.z, parameter.position.z]);
+        } else expect(positionResult.z).toEqual(0);
+
+        if (parameter.velocity.x !== 0) {
+          expect(tolerance - Math.abs(velocityResult.x / parameter.velocity.x - 1))
+            .toBeGreaterThanOrEqual(0, [velocityResult.x, parameter.velocity.x]);
+        } else expect(velocityResult.x).toEqual(0);
+
+        if (parameter.velocity.y !== 0) {
+          expect(tolerance - Math.abs(velocityResult.y / parameter.velocity.y - 1))
+            .toBeGreaterThanOrEqual(0, [velocityResult.y, parameter.velocity.y]);
+        } else expect(velocityResult.y).toEqual(0);
+
+        if (parameter.velocity.z !== 0) {
+          expect(tolerance - Math.abs(velocityResult.z / parameter.velocity.z - 1))
+            .toBeGreaterThanOrEqual(0, [velocityResult.z, parameter.velocity.z]);
+        } else expect(velocityResult.z).toEqual(0);
+      });
+    });
+  })
 });
