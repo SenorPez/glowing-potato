db2e0e7970d433870babe9084f6f453562708b1d
SenorPez <senorpez@senorpez.com>
2021-08-24 08:49:55 -0500

    DOM testing for progress component

--- orbit-viewer/src/app/progress/progress.component.spec.ts
+++ orbit-viewer/src/app/progress/progress.component.spec.ts
@@ -1,25 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProgressComponent } from './progress.component';
+import {HarnessLoader} from "@angular/cdk/testing";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {TestbedHarnessEnvironment} from "@angular/cdk/testing/testbed";
+import {MatProgressBarHarness} from "@angular/material/progress-bar/testing";
 
-describe('ProgressComponent', () => {
+describe('ProgressComponent DOM testing', () => {
   let component: ProgressComponent;
   let fixture: ComponentFixture<ProgressComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ProgressComponent ]
-    })
-    .compileComponents();
+    await TestBed
+      .configureTestingModule({
+        imports: [MatProgressBarModule],
+        declarations: [ProgressComponent]
+      })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProgressComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined('should be created');
+  });
+
+  it('mat-progress-bar should be indeterminate', async () => {
+    const divLoader = await loader.getChildLoader('#progress');
+    const progressBar = await divLoader.getHarness(MatProgressBarHarness);
+    expect(await progressBar.getMode()).toBe("indeterminate");
   });
 });
