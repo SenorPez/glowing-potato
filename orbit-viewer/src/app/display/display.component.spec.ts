68ce2e8cfa2c7ac8b09c1daf5aee2efccf7def03
SenorPez <senorpez@senorpez.com>
2021-08-23 23:35:46 -0500

    DOM testing for Display component

--- orbit-viewer/src/app/display/display.component.spec.ts
+++ orbit-viewer/src/app/display/display.component.spec.ts
@@ -1,7 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DisplayComponent } from './display.component';
-import {MatCheckboxChange} from "@angular/material/checkbox";
+import {DisplayComponent} from './display.component';
+import {MatCheckboxChange, MatCheckboxModule} from "@angular/material/checkbox";
+import {TestbedHarnessEnvironment} from "@angular/cdk/testing/testbed";
+import {HarnessLoader} from "@angular/cdk/testing";
+import {MatCheckboxHarness} from "@angular/material/checkbox/testing";
 
 describe('DisplayComponent class', () => {
   it('should emit true state for orbit check box', () => {
@@ -51,26 +54,51 @@ describe('DisplayComponent class', () => {
     component.transfersEvent.subscribe((state: boolean) => expect(state).toBeFalsy('should be false'));
     component.changeTransfers(event);
   });
-})
+});
 
-// describe('DisplayComponent', () => {
-//   let component: DisplayComponent;
-//   let fixture: ComponentFixture<DisplayComponent>;
-//
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ DisplayComponent ]
-//     })
-//     .compileComponents();
-//   });
-//
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(DisplayComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
-//
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+describe('DisplayComponent DOM testing', () => {
+  let component: DisplayComponent;
+  let fixture: ComponentFixture<DisplayComponent>;
+  let loader: HarnessLoader;
+
+  beforeEach(async () => {
+    await TestBed
+      .configureTestingModule({
+        imports: [MatCheckboxModule],
+        declarations: [DisplayComponent]})
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DisplayComponent);
+    loader = TestbedHarnessEnvironment.loader(fixture);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeDefined('should be defined');
+  });
+
+  it('orbits mat-checkbox should toggle from true to false', async () => {
+    const checkbox: MatCheckboxHarness = await loader.getHarness(MatCheckboxHarness.with({label: "Orbits"}));
+    expect(checkbox.isChecked()).toBeTruthy('should be true');
+    expect(component.orbitsEvent.subscribe((state: boolean) => expect(state).toBeFalsy('should be false')));
+    await checkbox.toggle();
+  });
+
+  it('transfers mat-checkbox should toggle from true to false', async () => {
+    const checkbox: MatCheckboxHarness = await loader.getHarness(MatCheckboxHarness.with({label: "Transfers"}));
+    expect(checkbox.isChecked()).toBeTruthy('should be true');
+    expect(component.orbitsEvent.subscribe((state: boolean) => expect(state).toBeFalsy('should be false')));
+    await checkbox.toggle();
+  });
+
+  it('planet locators mat-checkbox should toggle from true to false', async () => {
+    const checkbox: MatCheckboxHarness = await loader.getHarness(MatCheckboxHarness.with({label: "Planet Locators"}));
+    expect(checkbox.isChecked()).toBeTruthy('should be true');
+    expect(component.orbitsEvent.subscribe((state: boolean) => expect(state).toBeFalsy('should be false')));
+    await checkbox.toggle();
+  });
+});
+
